5b562c9c065a389d803b370df39d4fd93c93f14f
Murhaf Sousli <murhafsousli@gmail.com>
2018-10-28 00:27:22 +0300

    Update

--- projects/ngx-scrollbar/src/public_api.ts
+++ projects/ngx-scrollbar/src/public_api.ts
@@ -1,2 +1,3 @@
-export { ScrollbarModule } from './lib/scrollbar.module';
-export { ScrollbarComponent } from './lib/scrollbar.component';
+export * from './lib/scrollbar.module';
+export * from './lib/scrollbar.component';
+export * from './lib/smooth-scroll';
