c1cf978fee204f3e6b1f72a6dc6534d818f820de
Murhaf Sousli <murhafsousli@gmail.com>
2018-11-15 00:57:25 +0300

    General refactor

--- projects/ngx-scrollbar/src/scrollbar/ng-scrollbar.module.ts
+++ projects/ngx-scrollbar/src/scrollbar/ng-scrollbar.module.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { LayoutModule } from '@angular/cdk/layout';
 import { SmoothScrollModule } from '../smooth-scroll/smooth-scroll.module';
@@ -10,8 +10,8 @@ import { NgScrollbarThumb } from './ng-scrollbar-thumb';
   imports: [
     CommonModule,
     ScrollingModule,
-    LayoutModule,
-    SmoothScrollModule
+    SmoothScrollModule,
+    LayoutModule
   ],
   declarations: [
     NgScrollbar,
