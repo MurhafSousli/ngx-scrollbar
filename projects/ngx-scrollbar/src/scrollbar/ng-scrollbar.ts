f450182efd801c015914b7970e1b20af494170ab
Marc Ruiz <marcruiz.cat@gmail.com>
2018-12-03 18:29:19 -0800

    Avoid enable execution if disabled

--- projects/ngx-scrollbar/src/scrollbar/ng-scrollbar.ts
+++ projects/ngx-scrollbar/src/scrollbar/ng-scrollbar.ts
@@ -101,13 +101,13 @@ export class NgScrollbar implements AfterViewInit, OnDestroy {
   ngAfterViewInit() {
     this.view = this.scrollable.getElementRef().nativeElement;
 
-    if (this.disableOnBreakpoints) {
+    if (this.disableOnBreakpoints && !this.disabled) {
       // Enable/Disable custom scrollbar on breakpoints (Used to disable scrollbars on mobile phones)
       this._breakpointSub$ = this.breakpointObserver.observe(this.disableOnBreakpoints).pipe(
         // filter(() => this.viewInitialized),
         tap((result: BreakpointState) => result.matches ? this.disable() : this.enable())
       ).subscribe();
-    } else {
+    } else if (!this.disabled) {
       this.enable();
     }
 
