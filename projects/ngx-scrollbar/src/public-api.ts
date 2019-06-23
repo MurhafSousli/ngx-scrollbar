c3470d2a06555e5717915097501563b60380218e
Murhaf Sousli <murhafsousli@gmail.com>
2019-06-23 05:49:19 +0300

    Upgrade to Angular 8, closes #161

---
+++ projects/ngx-scrollbar/src/public-api.ts
@@ -0,0 +1,15 @@
+/*
+ * Public API Surface of ngx-scrollbar
+ */
+
+export * from './lib/scrollbar/ng-scrollbar.module';
+export * from './lib/scrollbar/ng-scrollbar-config';
+export * from './lib/scrollbar/ng-scrollbar';
+export * from './lib/scrollbar/custom-scroll-view';
+
+export * from './lib/smooth-scroll/smooth-scroll.module';
+export * from './lib/smooth-scroll/smooth-scroll';
+
+export * from './lib/reached-event/ng-scrollbar-reached';
+export * from './lib/reached-event/ng-scrollbar-reached-config';
+export * from './lib/reached-event/ng-scrollbar-reached.module';
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,66 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NavigationToolbarComponent } from './navigation-toolbar/navigation-toolbar.component';
+import {
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatExpansionModule, MatInputModule, MatListModule,
+  MatSlideToggleModule, MatTabsModule,
+  MatToolbarModule,
+  MatChipsModule, MatIconModule
+} from '@angular/material';
+import { LogoComponent } from './logo/logo.component';
+import { AuthorComponent } from './author/author.component';
+import { ResizeSensorModule } from '../../../projects/ngx-scrollbar/src/lib/resize-sensor/resize-sensor.module';
+import { NgScrollbarModule } from '../../../projects/ngx-scrollbar/src/lib/scrollbar/ng-scrollbar.module';
 
 @NgModule({
-  exports: [HeaderComponent, FooterComponent],
-  declarations: [HeaderComponent, FooterComponent]
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    ResizeSensorModule,
+    NgScrollbarModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    NavigationToolbarComponent,
+    LogoComponent,
+    MatCardModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+    MatToolbarModule,
+    MatListModule,
+    MatTabsModule,
+    MatInputModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    ResizeSensorModule
+  ],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavigationToolbarComponent,
+    LogoComponent,
+    AuthorComponent
+  ]
 })
 export class SharedModule {
 }
