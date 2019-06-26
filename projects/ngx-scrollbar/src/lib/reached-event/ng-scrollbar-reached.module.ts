b6fa23dec20748fb0291df31deb2f27c2f7535ad
Murhaf Sousli <murhafsousli@gmail.com>
2019-06-26 22:48:46 +0300

    refactor: Use a unique directive for each reached event

--- projects/ngx-scrollbar/src/lib/reached-event/ng-scrollbar-reached.module.ts
+++ projects/ngx-scrollbar/src/lib/reached-event/ng-scrollbar-reached.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { NgScrollbarModule } from '../scrollbar/ng-scrollbar.module';
-import { NgScrollbarReached } from './ng-scrollbar-reached';
+import {
+  NgScrollbarReachedBottom,
+  NgScrollbarReachedLeft,
+  NgScrollbarReachedRight,
+  NgScrollbarReachedTop
+} from './ng-scrollbar-reached';
 
 @NgModule({
   imports: [
     NgScrollbarModule
   ],
   declarations: [
-    NgScrollbarReached
+    NgScrollbarReachedTop,
+    NgScrollbarReachedBottom,
+    NgScrollbarReachedRight,
+    NgScrollbarReachedLeft
   ],
   exports: [
-    NgScrollbarReached
+    NgScrollbarReachedTop,
+    NgScrollbarReachedBottom,
+    NgScrollbarReachedRight,
+    NgScrollbarReachedLeft
   ]
 })
 export class NgScrollbarReachedModule {
--- projects/ngx-scrollbar/src/lib/reached-event/ng-scrollbar-reached.ts
+++ projects/ngx-scrollbar/src/lib/reached-event/ng-scrollbar-reached.ts
@@ -1,139 +1,143 @@
-import { Directive, NgZone, Optional, Input, Output, OnDestroy, AfterViewInit } from '@angular/core';
-import { Observable, Observer, Subject } from 'rxjs';
-import { filter, map, tap, distinctUntilChanged, takeUntil } from 'rxjs/operators';
-import { pipeFromArray } from 'rxjs/internal/util/pipe';
-
-import { NgScrollbarReachedChecker, NgScrollbarReachedPoint } from './ng-scrollbar-reached-config';
+import { Directive, Optional, Input, Output, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { Observable, Subject, Subscription, Observer } from 'rxjs';
+import { filter, map, tap, distinctUntilChanged } from 'rxjs/operators';
 import { NgScrollbar } from '../scrollbar/ng-scrollbar';
 
-@Directive({
-  selector: '[reachedTop], [reachedLeft], [reachedRight], [reachedBottom]',
-})
-export class NgScrollbarReached implements AfterViewInit, OnDestroy {
+class ScrollReached implements OnDestroy {
+
+  /** offset: Reached offset value in px */
+  @Input('reachedOffset') offset = 0;
 
   /**
    * scrollEvent: is a stream that emits scroll event when `elementScrolled()` is ready.
    * NOTE: I used this method because the scroll event must be initialized before the reached outputs,
    * but `elementScrolled()` returns undefined if called before the view is initialized.
    */
-  private verticalScrollEvent = new Subject<any>();
-  private horizontalScrollEvent = new Subject<any>();
+  protected scrollEvent = new Subject<any>();
 
-  /** Scroll event unsubscriber */
-  private unsubscriber = new Subject();
+  protected subscription = Subscription.EMPTY;
 
-  /**
-   * offset: Reached offset value in px
-   */
-  @Input('ngScrollbarOffset') offset = 0;
+  protected reachedEvent = new Observable((observer: Observer<any>) =>
+    this.scrollReached().subscribe(_ =>
+      Promise.resolve().then(() => this.ngZone.run(() => observer.next(_)))));
 
-  @Output() reachedTop: Observable<any> =
-    new Observable((observer: Observer<any>) =>
-      this.reached('top').subscribe(_ =>
-        Promise.resolve().then(() => this.ngZone.run(() => observer.next(_)))));
-
-  @Output() reachedLeft: Observable<any> =
-    new Observable((observer: Observer<any>) =>
-      this.reached('left').subscribe(_ =>
-        Promise.resolve().then(() => this.ngZone.run(() => observer.next(_)))));
-
-  @Output() reachedRight: Observable<any> =
-    new Observable((observer: Observer<any>) =>
-      this.reached('right').subscribe(_ =>
-        Promise.resolve().then(() => this.ngZone.run(() => observer.next(_)))));
-
-  @Output() reachedBottom: Observable<any> =
-    new Observable((observer: Observer<any>) =>
-      this.reached('bottom').subscribe(_ =>
-        Promise.resolve().then(() => this.ngZone.run(() => observer.next(_)))));
-
-  constructor(
-    @Optional() protected scrollbar: NgScrollbar,
-    protected checker: NgScrollbarReachedChecker,
-    protected ngZone: NgZone
-  ) {
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
     if (!scrollbar) {
       throw new Error('[NgScrollbarReached Directive]: Host element must be an NgScrollbar component.');
     }
   }
 
-  ngAfterViewInit() {
-    this.scrollbar.verticalScrolled.pipe(
-      takeUntil(this.unsubscriber),
-      tap((e: any) => this.verticalScrollEvent.next(e))
-    ).subscribe();
-
-    this.scrollbar.horizontalScrolled.pipe(
-      takeUntil(this.unsubscriber),
-      tap((e: any) => this.horizontalScrollEvent.next(e))
-    ).subscribe();
-  }
-
   ngOnDestroy() {
-    this.unsubscriber.next();
-    this.unsubscriber.complete();
+    this.subscription.unsubscribe();
   }
 
-  /**
-   *
-   * @param eventName: Name of event 'top', 'left', 'bottom', 'right'
-   */
-  reached(eventName: NgScrollbarReachedPoint): Observable<any> {
+  protected isReached(offset: number, e?: any): boolean {
+    return false;
+  }
+
+  protected scrollReached(): Observable<any> {
     // current event
     let currEvent: any;
 
-    // Get the proper reached function which checks if the scroll event has reached the target
-    const isReached = this.checker.getReachedCheckerFunc(eventName);
-
-    // Currently, 'pipeFromArray' is used to make it possible to pass an operator in a variable
-    return pipeFromArray([
+    return this.scrollEvent.pipe(
       tap((e) => currEvent = e),
       // Check if it scroll has reached
-      isReached(this.offset),
-      // Emit only if reached state has changed
+      map((e) => this.isReached(this.offset, e)),
+      // Distinct until reached value has changed
       distinctUntilChanged(),
-      // Emit only if it scroll has reached
+      // Emit only if reached is true
       filter((reached: boolean) => reached),
       // Return scroll event
       map(() => currEvent)
-    ])(eventName === 'top' || eventName === 'bottom' ? this.verticalScrollEvent : this.horizontalScrollEvent);
+    );
   }
-
 }
 
-/**
- *
- // const reachedStream = this.scrollEvent.pipe(
- //   map((event: any) => {
-    //     currEvent = event;
-    //     // Get the proper checker function 'top', 'left', 'bottom', 'right'
-    //     const reachedChecker = this.checker.getReachedCheckerFunc(eventName);
-    //     // Return reached value
-    //     return reachedChecker(event.target, this.offset);
-    //   }),
- //   // Emit only if reached value has changed
- //   distinctUntilChanged()
- // );
- */
-
-/**
- *  The following code work perfectly to return the reached event.
- *  However, because 'offset' input is introduced, the scroll event may skip emitting for every pixel depends on user scroll speed.
- *  Therefore, we needed to use '>=' condition to check if element scrollTop has reached the target
- *  And emit again only if reached value has changed
- *
- interface ReachedState {
-    reached: boolean;
-    event: any;
+class VerticalScrollReached extends ScrollReached implements OnInit {
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
+    super(scrollbar, ngZone);
   }
 
- return this.scrollEvent.pipe(
- map((event: any) => {
-      const reachedChecker = this.checker.getPointChecker(point);
-      const reached = reachedChecker(event.target, this.offset);
-      return {reached, event}
-    }),
- filter((state: ReachedState) => state.reached),
- map((state) => state.event)
- );
- */
+  ngOnInit() {
+    this.subscription = this.scrollbar.verticalScrolled.subscribe(this.scrollEvent);
+  }
+}
+
+class HorizontalScrollReached extends ScrollReached implements OnInit {
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
+    super(scrollbar, ngZone);
+  }
+
+  ngOnInit() {
+    this.subscription = this.scrollbar.horizontalScrolled.subscribe(this.scrollEvent);
+  }
+}
+
+@Directive({
+  selector: '[reachedTop], [reached-top]',
+})
+export class NgScrollbarReachedTop extends VerticalScrollReached {
+  @Output() reachedTop: Observable<any> = this.reachedEvent;
+
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
+    super(scrollbar, ngZone);
+  }
+
+  protected isReached(offset: number, e: any): boolean {
+    const position = e.target.scrollTop;
+    return position <= offset;
+  }
+}
+
+@Directive({
+  selector: '[reachedBottom], [reached-bottom]',
+})
+export class NgScrollbarReachedBottom extends VerticalScrollReached {
+
+  @Output() reachedBottom: Observable<any> = this.reachedEvent;
+
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
+    super(scrollbar, ngZone);
+  }
+
+  protected isReached(offset: number, e: any): boolean {
+    const position = e.target.scrollTop + e.target.clientHeight;
+    const target = e.target.scrollHeight;
+    return position >= target - offset;
+  }
+}
+
+@Directive({
+  selector: '[reachedLeft], [reached-left]',
+})
+export class NgScrollbarReachedLeft extends HorizontalScrollReached {
+
+  @Output() reachedLeft: Observable<any> = this.reachedEvent;
+
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
+    super(scrollbar, ngZone);
+  }
+
+  protected isReached(offset: number, e: any): boolean {
+    const position = e.target.scrollLeft;
+    return position <= offset;
+  }
+}
+
+@Directive({
+  selector: '[reachedRight], [reached-right]',
+})
+export class NgScrollbarReachedRight extends HorizontalScrollReached {
+
+  @Output() reachedRight: Observable<any> = this.reachedEvent;
+
+  constructor(@Optional() protected scrollbar: NgScrollbar, protected ngZone: NgZone) {
+    super(scrollbar, ngZone);
+  }
+
+  protected isReached(offset: number, e: any): boolean {
+    const position = e.target.scrollLeft + e.target.clientWidth;
+    const target = e.target.scrollWidth;
+    return position >= target - offset;
+  }
+}
