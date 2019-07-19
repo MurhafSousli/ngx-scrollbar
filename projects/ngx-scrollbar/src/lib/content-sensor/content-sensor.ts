4909897114b031ba9be3ca9ecf90bc37ed346193
Murhaf Sousli <murhafsousli@gmail.com>
2019-07-19 11:28:28 +0300

    Refactor content sensor directive

--- projects/ngx-scrollbar/src/lib/content-sensor/content-sensor.ts
+++ projects/ngx-scrollbar/src/lib/content-sensor/content-sensor.ts
@@ -1,38 +1,63 @@
-import { NgModule, Directive, Optional, AfterContentInit, OnDestroy, NgZone } from '@angular/core';
-import { ContentObserver, ObserversModule } from '@angular/cdk/observers';
-import { Subscription } from 'rxjs';
+import { NgModule, Directive, Optional, AfterContentInit, OnDestroy, NgZone, Input, Inject } from '@angular/core';
+import { Platform, PlatformModule } from '@angular/cdk/platform';
+import { Observable, Observer, Subscription } from 'rxjs';
+import { debounceTime, tap } from 'rxjs/operators';
 import { NgScrollbar } from '../scrollbar/ng-scrollbar';
+import { NG_SCROLLBAR_DEFAULT_OPTIONS, NgScrollbarDefaultOptions } from '../scrollbar/ng-scrollbar-config';
 
 @Directive({
   selector: '[content-sensor], [contentSensor]'
 })
 export class ContentSensor implements AfterContentInit, OnDestroy {
 
-  subscription = Subscription.EMPTY;
+  private subscription = Subscription.EMPTY;
+
+  @Input() sensorDebounce: number = this.defaultOptions.contentObserverDebounce;
 
   constructor(private ngZone: NgZone,
-              private contentObserver: ContentObserver,
-              @Optional() private scrollbar: NgScrollbar) {
+              private platform: Platform,
+              @Optional() private scrollbar: NgScrollbar,
+              @Inject(NG_SCROLLBAR_DEFAULT_OPTIONS) private defaultOptions: NgScrollbarDefaultOptions) {
     if (!scrollbar) {
       throw new Error('[NgScrollbar Content Sensor Directive]: Host element must be an NgScrollbar component.');
     }
   }
 
   ngAfterContentInit() {
-    this.ngZone.runOutsideAngular(() =>
-      this.subscription = this.contentObserver.observe(this.scrollbar.view).subscribe(() => this.scrollbar.update())
-    );
+    this.ngZone.runOutsideAngular(() => {
+      if (this.platform.isBrowser) {
+        this.subscription = this.observer().pipe(
+          tap(() => this.scrollbar.update())
+        ).subscribe();
+      }
+    });
   }
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
   }
+
+  private observer(): Observable<any> {
+    return this.sensorDebounce ? this.observeContent().pipe(debounceTime(this.sensorDebounce)) : this.observeContent();
+  }
+
+  private observeContent(): Observable<any> {
+    let contentObserver: MutationObserver | any;
+    return new Observable((observer: Observer<any>) => {
+      contentObserver = new MutationObserver((e: any) => observer.next(e));
+      contentObserver.observe(this.scrollbar.view, {
+        characterData: true,
+        childList: true,
+        subtree: true
+      });
+    });
+  }
 }
 
 @NgModule({
-  imports: [ObserversModule],
+  imports: [PlatformModule],
   declarations: [ContentSensor],
   exports: [ContentSensor]
 })
-export class ContentSensorModule {
+export class NgScrollbarContentSensorModule {
 }
