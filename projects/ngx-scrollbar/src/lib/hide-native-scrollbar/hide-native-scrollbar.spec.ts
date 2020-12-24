53e302ef7ceb49367776a4ac3d8e6012a7ab7b21
Murhaf Sousli <murhafsousli@gmail.com>
2020-12-24 19:50:13 +0100

    Update unit tests

--- projects/ngx-scrollbar/src/lib/hide-native-scrollbar/hide-native-scrollbar.spec.ts
+++ projects/ngx-scrollbar/src/lib/hide-native-scrollbar/hide-native-scrollbar.spec.ts
@@ -26,36 +26,14 @@ describe('HideNativeScrollbar Directive', () => {
     fixture.detectChanges();
   });
 
-  afterEach(() => {
-    fixture.destroy();
-  });
-
   it('should create hideNativeScrollbar directive', () => {
     expect(directiveElement).toBeDefined();
   });
 
-  it('should set the native scrollbar size to the CSS variable', (doneFn: DoneFn) => {
-    setTimeout(() => {
-      const el: HTMLElement = directiveElement.nativeElement;
-      const size = getComputedStyle(el).getPropertyValue('--native-scrollbar-size');
-      expect(size).toBe(getOS() === 'mac' ? '0px' : '16px');
-      doneFn();
-    }, 50);
+  it('should set the native scrollbar size to the CSS variable', () => {
+    const el: HTMLElement = directiveElement.nativeElement;
+    const size = getComputedStyle(el).getPropertyValue('--native-scrollbar-size');
+    expect(size).toBe('0px');
   });
 });
 
-function getOS() {
-  const platform = window.navigator.platform;
-  const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-  const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-  let os = null;
-
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'mac';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'windows';
-  } else if (!os && /Linux/.test(platform)) {
-    os = 'linux';
-  }
-  return os;
-}
--- projects/ngx-scrollbar/src/lib/hide-native-scrollbar/native-scrollbar-size-factory.spec.ts
+++ projects/ngx-scrollbar/src/lib/hide-native-scrollbar/native-scrollbar-size-factory.spec.ts
@@ -15,24 +15,8 @@ describe('NativeScrollSizeFactory Service', () => {
 
   it('should emit the native scrollbar size', (doneFn: DoneFn) => {
     service.scrollbarSize.subscribe((size: number) => {
-      expect(size).toBe(getOS() === 'mac' ? 0 : 16);
+      expect(size).toBe(0);
       doneFn();
     });
   });
 });
-
-function getOS() {
-  const platform = window.navigator.platform;
-  const macosPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
-  const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
-  let os = null;
-
-  if (macosPlatforms.indexOf(platform) !== -1) {
-    os = 'mac';
-  } else if (windowsPlatforms.indexOf(platform) !== -1) {
-    os = 'windows';
-  } else if (!os && /Linux/.test(platform)) {
-    os = 'linux';
-  }
-  return os;
-}
