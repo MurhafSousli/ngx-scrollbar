07afb7c44b9f67ce904d5273ac2f5407bdb725c4
Murhaf Sousli <murhafsousli@gmail.com>
2021-01-28 22:12:48 +0100

    update

--- karma.conf.js
+++ karma.conf.js
@@ -25,18 +25,14 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ngx-scrollbar'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' },
-        { type: 'lcov', dir: 'coverage/' }
+        { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
-    preprocessors: {
-      'src/app/**/*.js': ['coverage']
-    },
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
