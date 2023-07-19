f3f4000be481d1b96cf5a1b22ed65a02baca3480
Murhaf Sousli <murhafsousli@gmail.com>
2023-07-19 04:52:01 +0300

    Improve app config imports

--- projects/ngx-scrollbar-demo/src/main.ts
+++ projects/ngx-scrollbar-demo/src/main.ts
@@ -1,19 +1,14 @@
 import { APP_ID, enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { environment } from './environments/environment';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
 
 if (environment.production) {
   enableProdMode();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  bootstrapApplication(AppComponent, {
-    providers: [
-      { provide: APP_ID,  useValue: 'serverApp' },
-      provideAnimations()
-    ]
-  }).catch(err => console.error(err));
+  bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
 });
