7c07ea0a6f5dded317e82e173a60553a2d18a990
Murhaf Sousli <murhafsousli@gmail.com>
2025-06-09 20:37:15 +0200

    Update

--- projects/ngx-scrollbar-demo/src/app/home-page/home-page.component.ts
+++ projects/ngx-scrollbar-demo/src/app/home-page/home-page.component.ts
@@ -1,77 +1,24 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy, } from '@angular/core';
 import { Platform } from '@angular/cdk/platform';
-import { MatButton } from '@angular/material/button';
-import { ScrollbarDocument } from 'ngx-scrollbar';
-import { NgScrollReachDrop } from 'ngx-scrollbar/reached-event';
+import { LabComponent } from '../lab/lab.component';
+import { HeaderComponent } from '../shared/header/header.component';
 import { Example2Component } from '../example2/example2.component';
 import { Example3Component } from '../example3/example3.component';
 import { Example5Component } from '../example5/example5.component';
-import { LabComponent } from '../lab/lab.component';
-import { HeaderComponent } from '../shared/header/header.component';
-import { NgScrollbarAsyncViewport, NgScrollbar, NgScrollbarExt, NgScrollbarAnywhere, ScrollbarRef } from 'ngx-scrollbar';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
   imports: [
+    HeaderComponent,
+    LabComponent,
     Example2Component,
     Example3Component,
-    Example5Component,
-    LabComponent,
-    HeaderComponent,
-    MatButton,
-    NgScrollbarExt,
-    NgScrollbarAsyncViewport,
-    NgScrollReachDrop,
+    Example5Component
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HomePageComponent {
-  externalViewport = '.my-custom-viewport';
-  externalContentWrapper = '.my-custom-content-wrapper';
-  externalSpacer = '.my-custom-spacer';
-
-  show = false;
-
   readonly platform: Platform = inject(Platform);
-
-  readonly anyWhere = inject(NgScrollbarAnywhere);
-  readonly scrollbarDocument = inject(ScrollbarDocument);
-
-  flag = false;
-  reachedDisabled = false;
-  scrollbarRef: ScrollbarRef<NgScrollbar>;
-
-  x(t: NgScrollbarExt) {
-    console.log('🤖', t);
-  }
-
-  log(e) {
-    console.log('🐴', e);
-  }
-
-  test() {
-    if (this.flag) {
-      this.flag = false;
-      this.scrollbarRef?.destroy();
-      return;
-    }
-
-    this.flag = true;
-
-    requestAnimationFrame(() => {
-      this.scrollbarRef = this.anyWhere.createScrollbar('#test');
-    });
-    // requestAnimationFrame(() => {
-    //   this.scrollbarDocument.attachScrollbar();
-    // });
-
-    // requestAnimationFrame(() => {
-    //   this.anyWhere.createScrollbar({
-    //     host: '#test',
-    //     viewport: '.viewport'
-    //   });
-    // })
-  }
 }
--- projects/ngx-scrollbar/docs/Scrollbar-document.md
+++ projects/ngx-scrollbar/docs/Scrollbar-document.md
@@ -1,3 +1,24 @@
-# Scrollbar Document (experimental)
+The `NgScrollbarDocument` service is used to attach scrollbar on the root document element.
 
-This is a new feature that adds the custom scrollbars to the document element. 
+```ts
+import { Component, inject, afterNextRender } from '@angular/core';
+import { NgScrollbarDocument } from 'ngx-scrollbar';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+
+  scrollbarDocument = inject(NgScrollbarDocument);
+  
+  constructor() {
+    afterNextRender({
+      earlyRead: () => {
+        this.scrollbarDocument.attachScrollbar();
+      }
+    });
+  }
+}
+```
